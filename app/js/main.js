eed59e83a056e16c9595a70423a7e975240cac72
dmytrenkoyurii <dmitrenko.yura@gmail.com>
2020-03-20 21:45:28 +0200

    added without form and footer

---
+++ app/js/main.js
@@ -0,0 +1,35 @@
+$(function() {
+
+    $('.choose-slider').slick({
+        vertical: true,
+        verticalSwiping: true,
+        slidesToShow: 3,
+        autoplay: true,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 1,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+
+});
+
+// onclick = 'window.scrollBy(0, window.innerHeight)' > window.scrollBy(0, window.innerHeight)
